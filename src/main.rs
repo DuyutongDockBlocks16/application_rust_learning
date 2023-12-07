9ca30459c2e075aaabc12806a205b42eb894e2c6
YutongDu <yutong.du@aalto.fi>
2023-12-07 17:26:26 +0200

    增加了90%学分的代码内容

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,7 @@
-use application::{COWSAY, DRAGONSAY, TUXSAY};
-use clap::Parser;
-
-/// A simple cowsay clone
-#[derive(Parser)]
-#[clap(name = "application", about = "A simple cowsay clone")]
-struct Opts {
-    #[clap(short = 'a', long = "animal", default_value = "cow")]
-    animal: String,
-
-    #[clap(short = 'm', long = "message", default_value = "Moo!")]
-    message: String,
-}
-
-fn prepend_spaces(s: &str, n: usize) -> String {
-    let mut s = s.to_string();
-    s.insert_str(0, &" ".repeat(n));
-    s
-}
+// replace application with the name of your package
+use application::game;
 
 fn main() {
-    let opts = Opts::parse();
-    let Opts { animal, message } = opts;
-
-    let art = match animal.to_lowercase().as_str() {
-        "dragon" => DRAGONSAY,
-        "tux" => TUXSAY,
-        "cow" => COWSAY,
-        _ => panic!("Unknown animal: {message}"),
-    };
-
-    println!("< {} >", message);
-    let indent = |s| prepend_spaces(s, message.len() + 4);
-    let art = art
-        .trim()
-        .split('\n')
-        .map(indent)
-        .collect::<Vec<String>>()
-        .join("\n");
-    println!("{art}");
+    #[allow(unused)]
+        let mut game = game::Game::new();
 }
---
+++ src/unit/mod.rs
@@ -0,0 +1,9 @@
+pub mod player;
+pub mod collectible;
+pub mod enemy;
+pub mod wall;
+
+pub use player::Player;
+pub use collectible::Collectible;
+pub use enemy::Enemy;
+pub use wall::Wall;
